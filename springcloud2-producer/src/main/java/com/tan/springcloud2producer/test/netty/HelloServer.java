67563b152e2dc9f7ffd9f844c61907bcacf70a35
reboot-new <niuchenxi1123@163.com>
2019-12-20 17:56:54 +0800

    空格测试

--- springcloud2-producer/src/main/java/com/tan/springcloud2producer/test/netty/HelloServer.java
+++ springcloud2-producer/src/main/java/com/tan/springcloud2producer/test/netty/HelloServer.java
@@ -35,6 +35,7 @@ public class HelloServer {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
         }
+
     }
 
     public static void main(String[] args) throws Exception {
